ed4c2984ee0aa1d589c1a45d999c7a4a671b9e54
Saron <saronabebe2019@gmail.com>
2025-07-07 16:21:34 -0800

    Implement edit functionality

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -1,9 +1,30 @@
-import { useContext } from "react";
-import { Text, View } from "react-native";
+import { useContext, useState } from "react";
+import { Modal, Text, TextInput, View } from "react-native";
 import ListContext from "../context/ListContextDefinition.jsx";
 
 const TaskList = () => {
-  const { taskList, setTaskList } = useContext(ListContext);
+  const { setInput, input, taskList, setTaskList } = useContext(ListContext);
+  const [visible, setVisible] = useState(false);
+  const [editedTask, setEditedTask] = useState(null);
+
+  const handleEditPress = (task) => {
+    setInput(task.task);
+    setEditedTask(task);
+    setVisible(true);
+  };
+
+  const saveEdit = () => {
+    setTaskList((prevList) =>
+      prevList.map((task) =>
+        task.id === editedTask.id ? { ...task, task: input } : task
+      )
+    );
+
+    setInput("");
+    setVisible(false);
+    setEditedTask(null);
+  };
+
   return (
     <View
       style={{
@@ -68,6 +89,7 @@ const TaskList = () => {
                   paddingHorizontal: 16,
                   borderRadius: 16,
                 }}
+                onPress={() => handleEditPress(task)}
               >
                 Edit
               </Text>
@@ -85,6 +107,89 @@ const TaskList = () => {
           </View>
         ))
       )}
+      <Modal visible={visible} transparent={true} animationStyle="slide">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.5)", // semi-transparent background
+          }}
+        >
+          <View
+            style={{
+              gap: 16,
+              shadowOffset: { width: 1, height: 2 },
+              shadowColor: "black",
+              shadowOpacity: 0.4,
+              shadowRadius: 5,
+              backgroundColor: "white",
+              padding: 16,
+              width: 300,
+              height: 200,
+              borderRadius: 16,
+              elevation: 4, // Android shadow
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                marginVertical: 8,
+                fontWeight: "600",
+                fontSize: 20,
+              }}
+            >
+              Edit Task
+            </Text>
+            <TextInput
+              style={{
+                borderWidth: 1,
+                padding: 8,
+                borderColor: "black",
+                borderRadius: 16,
+              }}
+              value={input}
+              onChangeText={(newText) => setInput(newText)}
+            />
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: 16,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  backgroundColor: "lightskyblue",
+                  borderRadius: 16,
+                  paddingVertical: 8,
+                  paddingHorizontal: 16,
+                  color: "white",
+                  alignSelf: "center",
+                }}
+                onPress={() => saveEdit()}
+              >
+                Change
+              </Text>
+              <Text
+                style={{
+                  backgroundColor: "lightskyblue",
+                  borderRadius: 16,
+                  paddingVertical: 8,
+                  paddingHorizontal: 16,
+                  color: "white",
+                  alignSelf: "center",
+                }}
+                onPress={() => setVisible(false)}
+              >
+                Cancel
+              </Text>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
